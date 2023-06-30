fff9d04ba7fad743cd40bf6879470ab334df0e02
CheechRockwizard <github@lamcat.co.uk>
2023-06-30 15:58:00 +0100

    Create fullwidthpage.php

---
+++ fullwidthpage.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Full Width Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+$modal = 0;
+?>
+<main id="main" style="padding-top:130px">
+	<?php the_post();
+	the_content(); ?>
+</main>
+<?php
+get_footer();
+?>
